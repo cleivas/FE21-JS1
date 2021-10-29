96a46d65612888d985c5f0ebd02097725e8ced00
cleivas <clara.leivas@gmail.com>
2021-10-29 15:08:16 +0200

    added examples

---
+++ 001_okt_29_intro_till_js_och_DOM/first_example/js/script.js
@@ -0,0 +1,16 @@
+// This is a single line comment
+/*
+This is a multi line comment
+*/
+
+//loggar text till konsolen
+console.log("Hello, world!"); //en till kommentar
+
+console.log(600); //det här är ett nummer
+console.log(3*2); //loggar resultatet av uträkningen
+console.log("3*2"); //loggar texten 3*2
+
+//tre olika sätt att skriva text alla är okej, förutom i de sitationer när man måste använda backtick
+console.log('test'); //single quotation
+console.log("test"); //regular quotation
+console.log(`test`); //backtick 
