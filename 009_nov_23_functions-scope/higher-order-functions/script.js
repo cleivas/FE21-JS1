103b265c22ebd47e41c02886ebcf46e65b837f5f
cleivas <clara.leivas@gmail.com>
2022-01-24 18:24:54 +0100

    Cleaned up function example

--- 009_nov_23_functions-scope/higher-order-functions/script.js
+++ 009_nov_23_functions-scope/higher-order-functions/script.js
@@ -17,30 +17,31 @@ const numbersSquared = [];
 //Vi loopar igenom alla element i arrayen numbers
 //För varje element skickar vi det värdet som argument till funktionen square
 //Det returnerade värdet läggs sedan till till arrayen numbersSquared
-for(let i=0; i<numbers.length; i++){
-    const result = square( numbers[i])
-    numbersSquared.push( result );
-}
-console.log(numbersSquared);
+// for(let i=0; i<numbers.length; i++){
+//     const result = square( numbers[i])
+//     numbersSquared.push( result );
+// }
+// console.log(numbersSquared);
 
-const numbersTimesTwo = [];
-for(let i=0; i<numbers.length; i++){
-    const result = timesTwo( numbers[i])
-    numbersTimesTwo.push( result );
-}
-console.log(numbersTimesTwo);
+// const numbersTimesTwo = [];
+// for(let i=0; i<numbers.length; i++){
+//     const result = timesTwo( numbers[i])
+//     numbersTimesTwo.push( result );
+// }
+// console.log(numbersTimesTwo);
 
 
 const doMathOnArray = function(arr, func){
     const results = [];
     for(let i=0; i< arr.length; i++){
-        results.push( func(arr[i]) );
+        // results.push( func(arr[i]) );
+        arr[i] = func(arr[i]);
     }
-    return results;
+    return arr;
 }
+console.log(numbers);
+doMathOnArray(numbers, square);
+console.log(numbers);
 
-const numbersSquared2 = doMathOnArray(numbers, square);
-console.log(numbersSquared2);
-
-const numbersTimesTwo2 = doMathOnArray(numbers, timesTwo);
-console.log(numbersTimesTwo2);
+doMathOnArray(numbers, timesTwo);
+console.log(numbers);
